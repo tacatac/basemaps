67adccdb473f1b9049fa1f6096c2301d53ef02e4
taca <taca@kadisius.eu>
2019-07-12 23:45:45 +0200

    Add michelin2 style
    * add grasses, farmland and orchard stylings

---
+++ styles/michelin2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+michelin2 = {
+    'motorway_clr': '228 24 24',
+    'trunk_clr': '228 24 24',
+    'primary_clr': {
+        0:'"#aaaaaa"',
+        9:'228 24 24'
+    },
+    'secondary_clr': {
+        0: '"#aaaaaa"',
+        10: '252 241 20'
+    },
+    'tertiary_clr': {
+        0: '"#aaaaaa"',
+        12: '252 241 20'
+    },
+    'other_clr': {
+        0: '"#aaaaaa"',
+        14: '"#ffffff"'
+    },
+    'display_primary_outline': {
+        0: 0,
+        11: 1
+    },
+    'display_secondary_outline': {
+        0: 0,
+        12: 1
+    },
+    'display_tertiary_outline': {
+        0: 0,
+        13: 1
+    },
+    'display_other_outline': {
+        0: 0,
+        14: 1
+    },
+    'display_grasses': {
+        0: 0,
+        6: 1
+    },
+    'display_orchard': {
+        0: 0,
+        9: 1
+    },
+    'display_farmland': {
+        0: 0,
+        6: 1
+    },
+
+    'motorway_ol_width': 0.5,
+    'trunk_ol_width': 0.5,
+    'primary_ol_width': 0.2,
+    'secondary_ol_width': 0.2,
+    'tertiary_ol_width': 0.2,
+    'other_ol_width': 0.2,
+
+    'pedestrian_clr': '"#fafaf5"',
+    'forest_clr': '188 220 180',
+    'industrial_clr': '"#ebe5d9"',
+    'education_clr': '"#ded1ab"',
+    'hospital_clr': '"#e6c8c3"',
+    'residential_clr': '255 234 206',
+    'land_clr': '"#f1f1f1"',
+    'park_clr': '"#dcdcb4"',
+    'ocean_clr': '172 220 244',
+    'waterarea_clr': '172 220 244',
+    'river_clr': '172 220 244',
+    'stream_clr': '172 220 244',
+    'canal_clr': '172 220 244',
+
+    'motorway_ol_clr': '0 0 0',
+    'trunk_ol_clr': '0 0 0',
+    'primary_ol_clr': '0 0 0',
+    'secondary_ol_clr': '0 0 0',
+    'tertiary_ol_clr': '0 0 0',
+    'other_ol_clr': '0 0 0',
+    'pedestrian_ol_clr': '0 0 0',
+    'footway_clr': '"#7f7f7f"'
+}
